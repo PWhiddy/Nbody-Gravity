4ce18400f8e7641c169d9c147ed27a52ebcb54e9
David Gauchard <gauchard@laas.fr>
2019-06-04 11:31:01 +0200

    compilation script: minor changes for debugging ease

--- build.bash
+++ build.bash
@@ -1,6 +1,7 @@
 set -x
 opt="-O3 -march=native -mtune=native"
-#pg="-pg"
 #opt="-O2"
-g++ $pg -std=c++11 -Wall -Wextra $opt BarnzNhutt.cpp -c -o run.o -fopenmp
-g++ $pg run.o -o run -fopenmp
+#pg="-pg"
+#g="-g"
+g++ $pg $g -std=c++11 -Wall -Wextra $opt BarnzNhutt.cpp -c -o run.o -fopenmp
+g++ $pg $g run.o -o run -fopenmp
